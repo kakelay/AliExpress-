d3d1975ed4367c5ab0e33f3a087dee915537e6cd
kakelay <kakelay18052002@gmail.com>
2024-03-22 00:17:58 +0700

    update icons Shopping

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -2,17 +2,17 @@
     <div id="MainLayout" class="w-full fixed z-50">
         <div id="TopMenu" class="w-full bg-[#FAFAFA] border-b md:block hidden">
             <ul class="
-                    flex
-                    items-center
-                    justify-end 
-                    text-xs
-                    text-[#333333]
-                    font-light
-                    px-2
-                    h-10
-                    bg-[#FAFAFA]
-                    max-w-[1200px]
-                ">
+                                flex
+                                items-center
+                                justify-end 
+                                text-xs
+                                text-[#333333]
+                                font-light
+                                px-2
+                                h-10
+                                bg-[#FAFAFA]
+                                max-w-[1200px]
+                            ">
                 <li class=" border-r  border-r-gray-400   px-3 hover:text-[#FF4646]  cusor-pointer">
                     Sell on AliExpress
                 </li>
@@ -71,23 +71,60 @@
                 </NuxtLink>
                 <div class="max-w-[700px] w-full md:block hidden">
                     <div class="relative">
-                        <dev class="flex items-center border-2 border-[#FF4646] rounded-md w-full">
+                        <div class="flex items-center border-2 border-[#FF4646] rounded-md w-full">
                             <input class="
-                                w-full
-                                placeholder-gray-400
-                                text-sm
-                                pl-3
-                                focus:outline-none
-                            " placeholder="kitchen accessories" type="text" v-model="searchItem">
+                                            w-full
+                                            placeholder-gray-400
+                                            text-sm
+                                            pl-3
+                                            focus:outline-none
+                                        " placeholder="kitchen accessories" type="text" v-model="searchItem">
                             <Icon v-if="isSearching" name="eos-icons:loading" size="25" class="mr-2" />
                             <button class="flex items-center h-[100%] p-1.5 px-2 bg-[#FF4646]">
                                 <Icon name="ph:magnifying-glass" size="20" color="#ffffff" />
                             </button>
-
-
-                        </dev>
+                        </div>
+                        <div class="absolute bg-white max-w-[700px] h-auto w-full">
+                            <div v-if="false" class="p-1">
+                                <NuxtLink to="`/item/1`"
+                                    class="flex items-center justify-between w-full cursor-pointer hover:bg-gray-100 ">
+                                    <div class="flex items-center">
+                                        <img class="rounded-md" width="40" src="https://picsum.photos/id/82/300/320">
+                                        <div class="truncate ml-2">TESTING</div>
+                                    </div>
+                                    <div class="truncate">$ 98.99</div>
+                                </NuxtLink>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <NuxtLink to="/shoppingbcard" class="flex items-center">
+                    <button class="relative md:block hidden" @mouseenter="isCartHover = true"
+                        @mouseleave="isCartHover = false">
+                        <span class="
+                            absolute 
+                            flex 
+                            items-center 
+                            justify-center 
+                            -right-[3px] 
+                            top-0 
+                            bg-[#FF4646] 
+                            h-[17px] 
+                            min-w-[17px] 
+                            text-xs 
+                            text-white 
+                            px-0.5 
+                            rounded-full
+                            ">
+                            0
+                        </span>
+                        <div class="min-w-[40px]">
+                            <Icon name="ph:shopping-cart-simple-light" size="33" :color="isCartHover ? '#FF4646' : ''">
+                            </Icon>
+                        </div>
+
+                    </button>
+                </NuxtLink>
             </div>
         </div>
     </div>
@@ -95,6 +132,7 @@
 
 <script setup>
 let isAccountMenu = ref(false)
+let isCartHover = ref(false)
 let isSearching = ref(true)
 let searchItem = ref('')
 </script>
