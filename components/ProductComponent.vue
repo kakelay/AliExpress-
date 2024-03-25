f00f3f488b263f8a756f7be938ff259fe0338ac3
kakelay <kakelay18052002@gmail.com>
2024-03-26 00:37:17 +0700

    update images in file ProductComponent.ve

---
+++ components/ProductComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :id="`ProductComponent${product.id}`" class="
+        bg-white
+        inline-block
+        rounded
+        hover:shadow-[0_0__10px_3px_rgba(0,0,0,0.15)]
+        cursor-pointer
+    ">
+        <NuxtLink :to="`/item/${product.id}`">
+            <img class="rounded-t" :src="product.url">
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['product'])
+const { product } = toRefs(props)
+
+const priceComputed = computed(() => {
+    return product.value.price / 100
+})
+
+const oldPriceComputed = computed(() => {
+    let res = (product.value.price + (product.value.price / 20)) / 100
+    return res.toFixed(2)
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,34 @@
 <template>
   <MainLayout>
     <div d="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-        IndexPage
+      <div
+        class="grid max-xl:grid-flow-cols-6 lg:grid-flow-cols-5 md:grid-flow-cols-4 sm:grid-cols-3 grid-flow-cols-2 gap-4">
+          <div v-if="products" v-for="product in products" :key="product">
+            
+            <ProductComponent :product="product"/>
+          </div>
+      </div>
     </div>
   </MainLayout>
 
 </template>
 
 <script setup>
+ 
 import MainLayout from '~/layouts/MainLayout.vue';
+const products = [
+  { id: 1, title: "Title 1", description: "This is a drcription ", url: "https://picsum.photos/id/7/800/800", price: 9999 },
+  { id: 2, title: "Title 2", description: "This is a drcription ", url: "https://picsum.photos/id/71/800/800", price: 7767 },
+  { id: 3, title: "Title 3", description: "This is a drcription ", url: "https://picsum.photos/id/72/800/800", price: 7734 },
+  { id: 4, title: "Title 4", description: "This is a drcription ", url: "https://picsum.photos/id/73/800/800", price: 67878 },
+  { id: 5, title: "Title 5", description: "This is a drcription ", url: "https://picsum.photos/id/74/800/800", price: 2221 },
+  { id: 6, title: "Title 6", description: "This is a drcription ", url: "https://picsum.photos/id/75/800/800", price: 43444 },
+  { id: 7, title: "Title 7", description: "This is a drcription ", url: "https://picsum.photos/id/76/800/800", price: 6456 },
+  { id: 8, title: "Title 8", description: "This is a drcription ", url: "https://picsum.photos/id/77/800/800", price: 8008 },
+  { id: 9, title: "Title 9", description: "This is a drcription ", url: "https://picsum.photos/id/78/800/800", price: 44465 },
+  { id: 10, title: "Title 10", description: "This is a drcription ", url: "https://picsum.photos/id/79/800/800", price: 9999 },
+
+
+
+]
 </script>
