5087ca7223df6b1d7ddd22dcdcf5a363a0c91072
kakelay <kakelay18052002@gmail.com>
2024-04-28 13:08:55 +0700

    update item image grid images on home page update ProductComponent.vue update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <MainLayout>
-    <div d="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-      <div
-        class="grid max-xl:grid-flow-cols-6 lg:grid-flow-cols-5 md:grid-flow-cols-4 sm:grid-cols-3 grid-flow-cols-2 gap-4">
+    <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      
+      <div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
         <div v-if="products" v-for="product in products" :key="product">
           <!-- {{ product }} -->
           <ProductComponent :product="product" />
