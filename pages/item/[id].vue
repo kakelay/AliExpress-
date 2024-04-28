566fe95a97677556da93151423f2d8fc05a8a16d
kakelay <kakelay18052002@gmail.com>
2024-04-29 00:33:39 +0700

    update  mouseover

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -4,6 +4,13 @@
             <div class="md:flex gap-4 justify-between mx-auto w-full">
                 <div class="md:w-[40%]">
                     <img class="rounded-lg object-fit" src="https://picsum.photos/id/212/800/800">
+                    <div v-if="images[0] !== ''" class="flex items-center justify-center mt-2">
+                        <div v-for="image in images" :key="image">
+                            <img @mouseover="currentImage = image" @click="currentImage = image" width="70"
+                                class="rounded-md object-fit border-[3px] cursor-pointer"
+                                :class="currentImage === image ? 'border-[#FF5353]' : ''" :src="image">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -12,4 +19,23 @@
 
 <script setup>
 import MainLayout from '~/layouts/MainLayout.vue';
+let currentImage = ref(null)
+
+onMounted(() => {
+    watchEffect(() => {
+        currentImage.value = 'https://picsum.photos/id/77/800/800'
+        images.value[0] = 'https://picsum.photos/id/77/800/800'
+    })
+})
+
+const images = ref([
+    '',
+    'https://picsum.photos/id/212/800/800',
+    'https://picsum.photos/id/233/800/800',
+    'https://picsum.photos/id/165/800/800',
+    'https://picsum.photos/id/99/800/800',
+    'https://picsum.photos/id/144/800/800',
+
+])
+
 </script>
