298f4e298f6d8df86e7f4faaef351330b0934c77
kakelay <kakelay18052002@gmail.com>
2024-05-20 00:31:23 +0700

    update create shoppincart.vue change productItem to CartItem.vue update Title and price on page shoppingcart update funtion select item and add button icon delete

---
+++ pages/shoppingcart.vue
@@ -0,0 +1,47 @@
+<template>
+  <MainLayout>
+    <div id="ShoppingCartPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      <div v-if="false" class="h-[500px] flex items-center justify-center">
+        <div class="pt-20">
+          <img class="mx-auto" width="250" src="/cart-empty.png" />
+          <div class="text-xl text-center mt-4">No items yets?</div>
+          <div v-if="true" class="flex text-center">
+              <NuxtLink 
+              to="/auth" class="bg-[#FD374F] w-full text-white text-[21px] font-semibold p-1.5  rounded-full mt-4 ">
+                Sign in
+              </NuxtLink>
+          </div>
+        </div>
+      </div>
+      <div v-else class="md:flex gap-4 justify-between mx-auto w-full">
+          <div class="md:w-[65%]">
+              <div class="bg-gray-50 rounded-lg p-4">
+                 <div class="text-2xl font-bold mb-2">
+                  Shopping Cart(0)
+                 </div>
+              </div>
+              <div class="bg-[#FEEEEF] rounded-lg p-4 mt-4">
+                 <div class="text-red-500 font-bold "> Welcome Deel application on 1 item only </div>
+              </div>
+              <div id="Items" class="bg-gray-50 rounded-lg p-4 mt-4">
+                <div v-for="product in products">
+                    <CartItem
+                      :product="product"
+                      :selectedArray="selectedArray"
+                      @selectedRadio="onSelectedRadio"
+                    />
+                </div>
+              </div>
+          </div>
+      </div>
+    </div>
+  </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from "~/layouts/MainLayout.vue";
+const products = [
+  { id: 1, title: "Title 1", description: "This is a drcription ", url: "https://picsum.photos/id/7/800/800", price: 9999 },
+  { id: 2, title: "Title 2", description: "This is a drcription ", url: "https://picsum.photos/id/71/800/800", price: 7767 },
+]
+</script>
