6d1cf6079ca1f85ca41f0b3cad2b8c505d6b0130
kakelay <kakelay18052002@gmail.com>
2024-05-27 00:56:41 +0700

    update Delivery Address

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,12 +1,41 @@
 <template>
   <MainLayout>
-    <div id="CheckoutPage" class="mt-4 max-w-[1200px] mx-auto px-2"></div>
+    <div id="CheckoutPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      <div class="md:flex gap-4 justify-between mx-auto w-full">
+        <div class="md:w-[65%]">
+          <div class="bg-white rounded-lg p-4">
+            <div class="text-xl font-semibold mb-2">Shipping Address</div>
+            <div v-if="true">
+              <NuxtLink
+                to="address"
+                class="flex items-center pb-2 text-blue-500 hover:text-red-400"
+              >
+                <Icon name="mdi:plus" size="18" class="mr-2" />
+                Update Address
+              </NuxtLink>
+              <div class="pt-2 border-t">
+                <div class="underline pb-1">Delivery Address</div>
+                <ul class="text-xs">
+                  <li class="flex items-center gap-2">
+                    <div>Content name:</div>
+                    <div class="font-bold">
+                      {{ currentAddress.data.name }}
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </MainLayout>
 </template>
 
 <script setup>
 import MainLayout from "~/layouts/MainLayout.vue";
 import { useUserStore } from "~/stores/user";
+import "eos-icons/dist/css/eos-icons.css";
 const userStore = useUserStore();
 const route = useRoute();
 
@@ -36,9 +65,10 @@ watch(() => {
     };
 });
 
-const stripeInit = async ()=>{
-  
-}
+const stripeInit = async () => {};
+const pay = async () => {};
+const createOrder = async (stripeId) => {};
+const showError = (errorMsgText) => {};
 
 const products = [
   {
